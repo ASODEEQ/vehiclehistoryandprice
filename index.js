d474c345982a665f2612ae493684dade41c58ad3
ASODEEQ <atolagbeolalekan1@gmail.com>
2025-02-16 00:51:55 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('sade');
+console.log('kunle');
+console.info('we are in class')
+console.log('we are in class');
+console.error('we are in class')
+console.warn('we are in class');
+
+//this is one type of output command
+
+alert('mr pamilerin claims that js is interesting')
+alert('wow js is nice')
+
+
+
